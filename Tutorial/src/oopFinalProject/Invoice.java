5c14f331ee216c008a6bb77ca1edb0660b7a69ce
andrec4 <andrec4@091DS-1182-17.student.cssd.ab.ca>
2018-12-04 13:51:15 -0700

    Almost finished OOP final project. 12/4/2018

--- Tutorial/src/oopFinalProject/Invoice.java
+++ Tutorial/src/oopFinalProject/Invoice.java
@@ -3,13 +3,28 @@ import java.util.ArrayList;
 
 public class Invoice {
 
-	private ArrayList<String> items;
+	private ArrayList<LineItem> items;
+	private Customer theCustomer;
 	
 	public Invoice()	{
-	items = new ArrayList<String>();
+	items = new ArrayList<LineItem>();
+	theCustomer = new Customer();
 	}
 	
-	public add
+	public Invoice(Customer c) {
+		theCustomer = c;
+	}
+	
+	public void addToOrder(Product p,int qty)	{
+		LineItem item1 = new LineItem(qty,p);
+		items.add(item1);
+	}
+	
+	public void printInVoice()	{
+		for (int i=0;i<items.size();i++) {
+			System.out.println();
+		}
+	}
 }
 
 
--- Tutorial/src/oopFinalProject/LineItem.java
+++ Tutorial/src/oopFinalProject/LineItem.java
@@ -3,5 +3,20 @@ package oopFinalProject;
 public class LineItem {
 	
 	private int qty;
+	private Product theProduct;
+	
+	public LineItem() {
+		qty = 0;
+		theProduct = new Product();
+	}
+	
+	public LineItem(int q, Product p)	{
+		qty = q;
+		theProduct = p;
+	}
+	
+	public String toString()	{
+		return "Product: "+theProduct+" Quantity: "+qty;
+	}
 
 }
